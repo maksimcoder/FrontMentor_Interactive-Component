e0071b615647202768760785476e0048c3859439
Max <vip.devyatilov@mailru>
2021-01-27 15:52:23 +0300

    Wrote full script

--- script.js
+++ script.js
@@ -1,40 +1,72 @@
-'use strict';
-
-const input = document.querySelector('#rangeInput'),
-      span = document.querySelector('.views-text'),
-      step = input.getAttribute('step');
-
-input.addEventListener('input', () => {
-if (input.value == '1000') {
-    let newInput = input.value.slice(0, 1);
-    span.textContent = newInput + 'M';
-} else {
-    if (input.value >= '40' ) {
-        input.setAttribute('step', 50);
-    } else {
-        input.setAttribute('step', 10);
-    }
-
-    span.textContent = input.value + 'K';
-}
-
-});
-
-
-// function checkForStep(input) {
-//     switch (step) {
-//         case :
-            
-//             break;
-    
-//         default:
-//             break;
-//     }
-// }
-
-
-// 10K pageviews / $8 per month
+// - 10K pageviews / $8 per month
 // - 50K pageviews / $12 per month
 // - 100K pageviews / $16 per month
 // - 500k pageviews / $24 per month
 // - 1M pageviews / $36 per month
+
+'use strict';
+
+const input = document.querySelector('#rangeInput'),
+      span = document.querySelector('.views-text'),
+      price = document.querySelector('.price-amount'),
+      checkbox = document.querySelector('.checkbox');
+
+let pricingList = {
+    0: '8',
+    1: '12',
+    2: '16',
+    3: '24',
+    4: '36'
+  };
+
+
+span.textContent = '10K pageviews';
+price.textContent = `$${pricingList[0]}.00`;
+
+function getYearPrice(oldObj) {
+    const values = Object.values(oldObj);
+    let newArr = [];
+    values.forEach(item => {
+        if (item.length < 5) {
+            newArr.push((item * 12) - (item * 12 * 25 /100));
+        } else {
+            newArr.push((item * 12) - (item * 12 * 25 /100));
+        }
+    });
+    const newObj = Object.assign({}, newArr);
+    return newObj;
+}
+
+
+
+function changeRangeValue(range, viewers, pricing, isYear) {
+    let tempDB = getYearPrice(pricingList);
+    let monthList = pricingList,
+        yearList;
+    range.addEventListener('input', () => {
+        if (isYear.checked) {
+            yearList = tempDB;
+            pricingList = yearList;
+        } else {pricingList = monthList;}
+        if (range.value < 35 && range.value == 10) {
+            viewers.textContent = '10K pageviews';
+            pricing.textContent = `$${pricingList[0]}.00`;
+        } else if (range.value < 60 && range.value == 35){
+            span.textContent = '50K pageviews';
+            pricing.textContent = `$${pricingList[1]}.00`;
+        } else if (range.value < 85 && range.value == 60) {
+            span.textContent = '100K pageviews'; 
+            pricing.textContent = `$${pricingList[2]}.00`;   
+        } else if (range.value < 110 && range.value == 85) {
+            viewers.textContent = '500K pageviews';
+            pricing.textContent = `$${pricingList[3]}.00`;
+        } else if (range.value == 110) {
+            viewers.textContent = '1M pageviews';
+            pricing.textContent = `$${pricingList[4]}.00`;
+        } else {
+            viewers.textContent = 'error';
+        }
+    });
+}
+changeRangeValue(input, span, price, checkbox);
+
